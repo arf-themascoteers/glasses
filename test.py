ec2c7f23829b20e9dd3b26f796a0627a37236ad4
Arif <80994072+arf-themascoteers@users.noreply.github.com>
2021-12-05 17:32:24 +1100

    Done

--- test.py
+++ test.py
@@ -21,8 +21,7 @@ def test(device):
         pred = torch.argmax(y_hat, dim=1, keepdim=True)
         correct += pred.eq(y.data.view_as(pred)).sum()
         total += x.shape[0]
-
-    print(f'Total:{total}, Correct:{correct}, Accuracy:{correct/total*100:.2f}')
+        print(f'Total:{total}, Correct:{correct}, Accuracy:{correct/total*100:.2f}')
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
